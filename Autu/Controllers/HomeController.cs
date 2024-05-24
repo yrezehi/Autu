bf631fe78fa5cd06df32b5a96a99362495da743e
unknown <yrezehi@gmail.com>
2024-05-24 08:28:33 +0300

    setup: project

--- Autu/Controllers/HomeController.cs
+++ Autu/Controllers/HomeController.cs
@@ -6,27 +6,14 @@ namespace Autu.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        public HomeController() { }
+       
+        public IActionResult Index() =>
+            View();
+        
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        public IActionResult Privacy() =>
+            View();
     }
 }
